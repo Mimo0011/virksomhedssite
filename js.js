8aa3d124e7393fd930fb3682380940d24d07f114
marthafallman <mafa005@stud.kea.dk>
2024-12-12 15:13:40 +0100

    burger menu addded

--- js.js
+++ js.js
@@ -1,1 +1,6 @@
+const burgerMenu = document.getElementById("burgerMenu");
+const navLinks = document.getElementById("navLinks");
 
+burgerMenu.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
